a6cc0c738847b615d3a96b321d6ca04840a7ed86
Ryan S. McCoy <18177650+ryansmccoy@users.noreply.github.com>
2019-05-31 23:03:38 -0500

    teyes

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, Switch } from 'react-router-dom';
 
 import UsersList from './components/UsersList';
-import AddUser from './components/AddUser';
+import About from './components/About';
+import NavBar from './components/NavBar';
+import Form from './components/Form';
+import Logout from './components/Logout';
+import UserStatus from './components/UserStatus';
 
 
 class App extends Component {
@@ -12,9 +17,19 @@ class App extends Component {
       users: [],
       username: '',
       email: '',
+      title: 'TestDriven.io',
+      formData: {
+        username: '',
+        email: '',
+        password: ''
+      },
+      isAuthenticated: false,
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.logoutUser = this.logoutUser.bind(this);
   };
   componentDidMount() {
     this.getUsers();
@@ -42,27 +57,97 @@ class App extends Component {
     obj[event.target.name] = event.target.value;
     this.setState(obj);
   };
+  handleUserFormSubmit(event) {
+    event.preventDefault();
+    const formType = window.location.href.split('/').reverse()[0];
+    let data = {
+      email: this.state.formData.email,
+      password: this.state.formData.password,
+    };
+    if (formType === 'register') {
+      data.username = this.state.formData.username;
+    }
+    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
+    axios.post(url, data)
+    .then((res) => {
+      this.clearFormState();
+      window.localStorage.setItem('authToken', res.data.auth_token);
+      this.setState({ isAuthenticated: true, });
+      this.getUsers();
+    })
+    .catch((err) => { console.log(err); });
+  };
+  handleFormChange(event) {
+    console.log(this.state.formData);
+    const obj = this.state.formData;
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
+  clearFormState() {
+    this.setState({
+      formData: { username: '', email: '', password: '' },
+      username: '',
+      email: ''
+    });
+  };
+  logoutUser() {
+    window.localStorage.clear();
+    this.setState({ isAuthenticated: false });
+  };
   render() {
     return (
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-half">
-              <br/>
-              <h1 className="title is-1">All Users</h1>
-              <hr/><br/>
-              <AddUser
-                username={this.state.username}
-                email={this.state.email}
-                addUser={this.addUser}
-                handleChange={this.handleChange}
-              />
-              <br/><br/>
-              <UsersList users={this.state.users}/>
+      <div>
+        <NavBar
+          title={this.state.title}
+          isAuthenticated={this.state.isAuthenticated}
+        />
+        <section className="section">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-half">
+                <br/>
+                <Switch>
+                  <Route exact path='/' render={() => (
+                    <UsersList
+                      users={this.state.users}
+                    />
+                  )} />
+                  <Route exact path='/about' component={About}/>
+                  <Route exact path='/register' render={() => (
+                    <Form
+                      formType={'Register'}
+                      formData={this.state.formData}
+                      handleUserFormSubmit={this.handleUserFormSubmit}
+                      handleFormChange={this.handleFormChange}
+                      isAuthenticated={this.state.isAuthenticated}
+                    />
+                  )} />
+                  <Route exact path='/login' render={() => (
+                    <Form
+                      formType={'Login'}
+                      formData={this.state.formData}
+                      handleUserFormSubmit={this.handleUserFormSubmit}
+                      handleFormChange={this.handleFormChange}
+                      isAuthenticated={this.state.isAuthenticated}
+                    />
+                  )} />
+                  <Route exact path='/logout' render={() => (
+                    <Logout
+                      logoutUser={this.logoutUser}
+                      isAuthenticated={this.state.isAuthenticated}
+                    />
+                  )} />
+                  <Route exact path='/status' render={() => (
+                    <UserStatus
+                      isAuthenticated={this.state.isAuthenticated}
+                    />
+                  )} />
+                </Switch>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     )
   }
 };
--- services/client/src/components/__tests__/NavBar.test.jsx
+++ services/client/src/components/__tests__/NavBar.test.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer';
-import { MemoryRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import NavBar from '../NavBar';
+const NavBar = (props) => (
+  // eslint-disable-next-line
+  <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+    <section className="container">
+      <div className="navbar-brand">
+        <strong className="navbar-item">{props.title}</strong>
+        {/* new */}
+        <span
+          className="nav-toggle navbar-burger"
+          onClick={() => {
+            let toggle = document.querySelector(".nav-toggle");
+            let menu = document.querySelector(".navbar-menu");
+            toggle.classList.toggle("is-active"); menu.classList.toggle("is-active");
+          }}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </div>
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          <Link to="/" className="navbar-item">Home</Link>
+          <Link to="/about" className="navbar-item">About</Link>
+          <Link to="/status" className="navbar-item">User Status</Link>
+        </div>
+        <div className="navbar-end">
+          <Link to="/register" className="navbar-item">Register</Link>
+          <Link to="/login" className="navbar-item">Log In</Link>
+          <Link to="/logout" className="navbar-item">Log Out</Link>
+        </div>
+      </div>
+    </section>
+  </nav>
+)
 
-const title = 'Hello, World!';
-
-test('NavBar renders properly', () => {
-  const wrapper = shallow(<NavBar title={title}/>);
-  const element = wrapper.find('strong');
-  expect(element.length).toBe(1);
-  expect(element.get(0).props.children).toBe(title);
-});
-
-test('NavBar renders a snapshot properly', () => {
-  const tree = renderer.create(
-    <Router location="/"><NavBar title={title}/></Router>
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+export default NavBar;
